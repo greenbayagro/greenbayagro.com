77a93a8a7669849f117925ed55a43191201fb93e
darkwebplayer <amrithanadhm@gmail.com>
2021-12-10 19:32:31 +0530

    setuped

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,32 @@
-<section id="main" class="w-screen h-screen">
-    <h1 class="font-bold text-4xl mx-auto my-auto  text-center font-poppins">Green Bay Agro</h1>
-</section>
-<footer>
+<div id="page" class="w-screen h-screen overflow-hidden">
+    <div class="md:w-1/2 w-full  h-full px-5 py-10 md:m-0 flex justify-center items-center">
+       <div class="  bg-white h-full w-full bg-opacity-70 backdrop-blur-md shadow-2xl  p-5 flex flex-col justify-center items-center rounded-md">
+            <div class="flex  justify-center items-center ">
+                <div class=" w-24 p-5">
+            <img src="/favicon.png" alt="">
+        </div>
+        <div class="font-poppins font-bold p-3 text-2xl text-green-500">
+            Green Bay Agro
+        </div>
+            </div>
+        <div class="">
+            <h1 class="text-5xl font-bold font-poppins text-green-600 text-center">COMING SOOON!</h1>
+        </div>
+        
+       </div>
+       
+    </div>
+<!-- <footer>
     photo credits <a href='https://www.freepik.com/psd/background'>Background psd created by freepik - www.freepik.com</a>
-</footer>
+</footer> -->
+</div>
+
+
+<style>
+#page{
+    background: url("/bg.svg") no-repeat;
+   background-size: cover;
+   background-position: center;
+   
+}
+</style>
