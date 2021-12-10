02fd862e2b577d8bd482a4603cf381f3389b5183
darkwebplayer <amrithanadhm@gmail.com>
2021-12-10 16:26:25 +0530

    vercel check

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<section id="main" class="w-screen h-screen">
+    <h1 class="font-bold text-4xl mx-auto my-auto  text-center font-poppins">Green Bay Agro</h1>
+</section>
+<footer>
+    photo credits <a href='https://www.freepik.com/psd/background'>Background psd created by freepik - www.freepik.com</a>
+</footer>
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const config = {
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif']
+			}
+		}
+	},
+	plugins: []
+};
+
+module.exports = config;
